404ba090b4e1f5543b4948803dcd1f517b841a37
dishag07 <dishag422@gmail.com>
2024-10-15 10:06:24 -0400

    ok done

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ $(document).ready(function() {
         $('.event-description').toggle();
     });
 });
+
+
+$(document).ready(function() {
+    $('.trunk').on('mouseenter', function() {
+        $(this).find('.description').stop(true, true).fadeIn(200);
+    }).on('mouseleave', function() {
+        $(this).find('.description').stop(true, true).fadeOut(200);
+    });
+});
+
+
+$(document).ready(function() {
+    $('.event-heading').on('click', function() {
+        $(this).next('.event-details').stop(true, true).slideToggle(300);
+    });
+});
